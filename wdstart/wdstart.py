fbc51cef5ab8831c7d3b932444d1e76d53906406
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-12-20 02:45:35 +0530

    docstring for the wdstart.webdriver functions

--- wdstart/wdstart.py
+++ wdstart/wdstart.py
@@ -6,15 +6,31 @@ import drivers
 
 
 def webdriver(name, incognito=False, user_agent=None, profile_path=None):
-    """Starts and returns a Selenium Webdriver.
+    """
+    Starts and returns a wrapped Selenium Webdriver object.
 
-    Args:
-        name (str): Name of the webdriver to be started [Chrome, Firefox, PhantomJs, HTMLUnit].
-        user_agent (str): The user_agent string the webdriver should use.
-        profile_path (str): The path of the browser profile [only for Firefox and Chrome].
+    Starts a selenium webdriver according to the given specifications, 
+    and returns an object of a wrapper class around Selenium. To learn 
+    more about this wrapper class, refer to the docs of the `drivers` 
+    module.
 
-    Returns:
-        selenium.webdriver: A Selenium Webdriver according to the Args.
+    Parameters
+    ----------
+        name : str
+            Name of the webdriver to be started. Supported webdrivers 
+            are Chrome, Firefox, PhantomJs and HTMLUnit.
+        user_agent : str, optional
+            The `user_agent` string the webdriver should use.
+        profile_path : str, optional
+            The path of the browser profile (only for Firefox and Chrome).
+
+    Returns
+    -------
+        drivers.BaseDriver
+            A object of a class inherited from `drivers.BaseDriver`, 
+            such as `ChromeDriver`. This is a wrapper class around 
+            selenium webdriver object. For details, refer to the docs 
+            of the `drivers` module.
     """
 
     name = name.lower()
