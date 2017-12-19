31d4271552f4e8aa28382cdda69ad6ddad4bb2ba
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-12-19 20:48:19 +0530

    Fix errors

--- wdstart/drivers.py
+++ wdstart/drivers.py
@@ -1,50 +1,49 @@
-from urllib.request import urlopen
 from urllib.error import URLError
+from urllib.request import urlopen
 
+import helper
 from selenium import webdriver as WD
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import WebDriverException
-
+from selenium.webdriver.chrome.options import Options
 
 
 class BaseDriver:
-	def __init__(self, incognito=False, user_agent=None, profile_path=None):
-		self.driver = None
-		self.name = None
-		self.incognito = incognito
-		self.user_agent = user_agent
-		self.profile_path = profile_path
+    def __init__(self, incognito=False, user_agent=None, profile_path=None):
+        self.driver = None
+        self.name = None
+        self.incognito = incognito
+        self.agent = user_agent
+        self.profile_path = profile_path
 
     @property
     def user_agent(self):
-        agent = ''
         try:
             agent = self.driver.execute_script("return navigator.userAgent")
         except WebDriverException:
             self.driver.get('http://www.google.com')
-            agent = self.driver.execute_script('return navigator.userAgent') 
+            agent = self.driver.execute_script('return navigator.userAgent')
         return agent
 
-	def __start(self):
-		pass
+    def __start(self):
+        pass
 
 
 class ChromeDriver(BaseDriver):
-	def __start(self, name, incognito=False, user_agent=None, profile_path=None):
+    def __start(self):
+        self.name = 'Chrome'
         opt = Options()
 
-        if user_agent:
-            opt.add_argument('user-agent={}'.format(user_agent))
-        if profile_path:
-            opt.add_argument('user-data-dir={}'.format(profile_path))
+        if self.agent:
+            opt.add_argument('user-agent={}'.format(self.agent))
+        if self.profile_path:
+            opt.add_argument('user-data-dir={}'.format(self.profile_path))
 
-        # Some enhancements to the webdriver to be opened.
+        # Some enhancements to the WebDriver to be opened.
         opt.add_argument('--disable-notifications')
         prefs = {'profile.default_content_setting_values.notifications': 2}
         opt.add_experimental_option('prefs', prefs)
 
-        driver_path = find_binary_file('chromedriver')
+        driver_path = helper.find_binary_file('chromedriver')
         self.driver_path = driver_path or '$PATH'
         if driver_path:
             self.driver = WD.Chrome(driver_path, chrome_options=opt)
@@ -53,25 +52,27 @@ class ChromeDriver(BaseDriver):
 
 
 class FirefoxDriver(BaseDriver):
-    def __start(self, name, incognito=False, user_agent=None, profile_path=None):
+    def __start(self):
+        self.name = 'Firefox'
         if self.profile_path:
             fp = WD.FirefoxProfile(self.profile_path)
         else:
             fp = WD.FirefoxProfile()
 
-        if self.user_agent:
-            fp.set_preference('general.useragent.override', user_agent)
+        if self.agent:
+            fp.set_preference('general.useragent.override', self.agent)
 
         self.driver = WD.Firefox(fp)
 
 
 class HTMLUnitDriver(BaseDriver):
-	def __start(self):
+    def __start(self):
+        self.name = 'HTMLUnit'
         while True:
             try:
                 urlopen('http://localhost:4444/wd/hub/status')
             except URLError:
-                start_selenium_server()
+                helper.start_selenium_server()
             else:
                 break
 
@@ -79,16 +80,17 @@ class HTMLUnitDriver(BaseDriver):
         if self.user_agent:
             dcap['version'] = self.user_agent
 
-        driver = WD.Remote(command_executor="http://localhost:4444/wd/hub", desired_capabilities=dcap)
+        self.driver = WD.Remote(command_executor="http://localhost:4444/wd/hub", desired_capabilities=dcap)
 
 
 class PhantomJSDriver(BaseDriver):
-	def __start(self):
-        dcap = DesiredCapabilities.PHANTOMJS
-        if self.user_agent:
-            dcap["phantomjs.page.settings.userAgent"] = user_agent
+    def __start(self):
+        self.name = 'PhantomJS'
+        dcap = WD.DesiredCapabilities.PHANTOMJS
+        if self.agent:
+            dcap["phantomjs.page.settings.userAgent"] = self.agent
 
-        phantomjs_path = find_binary_file('phantomjs')
+        phantomjs_path = helper.find_binary_file('phantomjs')
         if phantomjs_path:
             self.driver = WD.PhantomJS(phantomjs_path, desired_capabilities=dcap)
         else:
